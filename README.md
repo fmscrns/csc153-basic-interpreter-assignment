05c365a5d3e053ebafd539fb51825ab69ad8328d
fmscrns <fmscrns@gmail.com>
2021-01-13 20:45:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# THE INSTRUCTIONS:
+
+The logical expressions we learned in mathematics involves values T for True and F
+for False, and basic operations such as ¬ for negation, ∨ for disjunction (or), and ∧ for
+conjunction (and). Example expression such as ¬F and this will resolve into T. Other
+examples are the following:
+• T ∨ F will resolve into T
+• F ∧ ¬T will resolve into F
+• ¬(T ∨ F) ∧ T will resolve into F
+Write a simple interpreter for the resolution of logical expressions. Input should be
+any valid logical expressions as string and will produce output of either T or F and
+can also handle spaces in the input. You can use symbols available in the keyboard for
+the operator such as exclamation mark (!) for negation, vertical bar (|) for disjunction,
+and ampersand (&) for conjuction.
+You can use the parsers you have written from the previous assignment to handle the
+parsing of lexemes of the logical expressions.
+Some example of the resolve interpreter function are shown below:
+> resolve "!F"
+T
+> resolve "T | F"
+T
+> resolve "F & !T"
+F
+> resolve "!(T | F) & T"
+F
+> resolve "T | F & F"
+T
+The above examples are demonstrated using a commandline interaction to conveniently present the examples. You may have a different way of implementation of
+the interpreter, depending on the features provided by your programming language
+as long it achieves the same purpose.
---
+++ resolve.py
@@ -0,0 +1,58 @@
+def resolve(_expr, target=0):
+    for or_item in _expr.split("|"):
+        or_result = True
+        for and_item in or_item.split("&"):
+            if and_item == "F":
+                or_result = False
+        if or_result == True:
+            return "T"
+    return "F"
+
+def simplify(_expr, target=0):
+    awaiting_operand = True
+    negator_count = 0
+    result = ""
+    while target < len(_expr):
+        if _expr[target] == "T" and awaiting_operand is True:
+            awaiting_operand = False
+            result += "T" if negator_count % 2 == 0 else "F"
+            negator_count = 0
+            target += 1
+        elif _expr[target] == "F" and awaiting_operand is True:
+            awaiting_operand = False
+            result += "F" if negator_count % 2 == 0 else "T"
+            negator_count = 0
+            target += 1
+        elif _expr[target] == "&" and awaiting_operand is False and target != len(_expr) - 1:
+            awaiting_operand = True
+            result += _expr[target]
+            target += 1
+        elif _expr[target] == "|" and awaiting_operand is False and target != len(_expr) - 1:
+            awaiting_operand = True
+            result += _expr[target]
+            target += 1
+        elif _expr[target] == "!" and awaiting_operand is True and target != len(_expr) - 1:
+            negator_count += 1
+            target += 1
+        elif _expr[target] == "(" and awaiting_operand is True and target != len(_expr) - 1:
+            awaiting_operand = False
+            sub_result = simplify(_expr, target=target+1)
+            if isinstance(sub_result, tuple):
+                sub_result, target = sub_result
+                result += sub_result
+            else:
+                return sub_result
+        elif _expr[target] == ")" and awaiting_operand is False:
+            return resolve(result), target + 1
+        else:
+            return "Syntax error. Try again."
+    return resolve(result), target + 1
+
+while True:
+    _input = input("resolve ")
+    if _input[0] == '"' and _input[-1:] == '"':
+        result = simplify(_input[1:-1])
+        print(result[0]) if isinstance(result, tuple) else print(result)
+    else:
+        print("Syntax error. Try again.")
+        
